17eceed5c47f80f91094d797794dbf88da74bd38
metaversemoon <99382573+metaversemoon@users.noreply.github.com>
2023-06-09 16:37:52 -0400

    add constants

---
+++ constants/index.tsx
@@ -0,0 +1,14 @@
+export const navLinks = [
+  {
+    id: 'home',
+    title: 'home',
+  },
+  {
+    id: 'profile',
+    title: 'profile',
+  },
+  {
+    id: 'nftDetails',
+    title: 'nftDetails',
+  },
+]
